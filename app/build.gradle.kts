034d0276ad55ca1fd7d0e4c4203379f96bb64fd9
laura <hernandez.sandoval.laura@gmail.com>
2023-12-08 22:20:00 -0600

    SingUp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.neatroots.instagramclonee"
-    compileSdk = 33
-
+    compileSdk = 34
+buildFeatures{
+    viewBinding = true
+}
     defaultConfig {
         applicationId = "com.neatroots.instagramclonee"
         minSdk = 26
@@ -41,8 +44,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
 }
--- app/src/main/java/com/neatroots/instagramclonee/SingUpActivity.kt
+++ app/src/main/java/com/neatroots/instagramclonee/SingUpActivity.kt
@@ -2,10 +2,38 @@ package com.neatroots.instagramclonee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.neatroots.instagramclonee.databinding.ActivitySingUpBinding
 
 class SingUpActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySingUpBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_up)
+        setContentView(binding.root)
+
+        binding.singUpBtn.setOnClickListener {
+            if(binding.name.editText?.text.toString().equals("")or
+                binding.email.editText?.text.toString().equals("")or
+                binding.password.editText?.text.toString().equals("")
+                ){
+                Toast.makeText(this@SingUpActivity, "Please fill the all Information", Toast.LENGTH_SHORT).show()
+            }else{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.email.editText?.text.toString(),
+                    binding.password.editText?.text.toString()
+                ).addOnCompleteListener {
+                    result->
+
+                    if (result.isSuccessful){
+                        Toast.makeText(this@SingUpActivity, "Login Successfully", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@SingUpActivity, result.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
